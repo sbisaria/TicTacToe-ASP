2c2b97ebe8098e710351583da6d675b78a9b5c01
Shubhangi <sbisaria@tavisca.com>
2017-08-31 20:04:56 +0530

    add TicTacToe in Asp

---
+++ TicTacToe/App.aspx
@@ -0,0 +1,56 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="App.aspx.cs" Inherits="TicTacToe.App" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        
+        <div style="margin-left:35%;" >
+            <h1 style="margin-left:12%">Tic-Tac-Toe</h1>
+            <asp:Panel ID="Panel1" runat="server"  BackColor="#00CCFF" BorderColor="#003399" Height="400px" Width="400px" EnableTheming="True">
+                <br />
+                <br />
+                <br />
+                <br />
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <br />
+                <br />
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;
+                <asp:Button ID="GameControl1" runat="server" Height="50px" width="50px" BackColor="White" BorderColor="#33CCFF" OnClick="Button_Click" />
+                &nbsp;&nbsp;<asp:Button ID="GameControl2" runat="server" Height="50px" width="50px" BackColor="White" BorderColor="#33CCFF" OnClick="Button_Click" />
+                &nbsp;
+                <asp:Button ID="GameControl3" runat="server" Height="50px" width="50px" BackColor="White" BorderColor="#33CCFF" OnClick="Button_Click" />
+                <br />
+                <br />
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;
+                <asp:Button ID="GameControl4" runat="server" Height="50px" width="50px" BackColor="White" BorderColor="#33CCFF" OnClick="Button_Click"/>
+                &nbsp;&nbsp;<asp:Button ID="GameControl5" runat="server" Height="50px" width="50px" BackColor="White" BorderColor="#33CCFF" OnClick="Button_Click"/>
+                &nbsp;&nbsp;<asp:Button ID="GameControl6" runat="server" Height="50px" width="50px" BackColor="White" BorderColor="#33CCFF" OnClick="Button_Click"/>
+                <br />
+                <br />
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <asp:Button ID="GameControl7" runat="server" Height="50px" width="50px" BackColor="White" BorderColor="#33CCFF" OnClick="Button_Click"/>
+                &nbsp;
+                <asp:Button ID="GameControl8" runat="server" Height="50px" width="50px" BackColor="White" BorderColor="#33CCFF" OnClick="Button_Click"/>
+                &nbsp;
+                <asp:Button ID="GameControl9" runat="server" Height="50px" width="50px" BackColor="White" BorderColor="#33CCFF" OnClick="Button_Click"/>
+                <br />
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <br /><br /><br /><br /><br />
+            
+            </asp:Panel>
+            <div>
+            <asp:Label ID="ResultLabel" runat="server" Visible="False" Font-Bold="true" Font-Size="Large" ForeColor="#fff"></asp:Label>
+            <br />
+            <asp:Button ID="Reset" runat="server" OnClick="Reset_Click" Text="Reset" />
+            
+        </div>
+        </div>
+
+    </form>
+</body>
+</html>
---
+++ TicTacToe/App.aspx.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TicTacToe
+{
+    public partial class App : System.Web.UI.Page
+    {
+        static int turnCount = 0;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button_Click(object sender, EventArgs e)
+        {
+            string currentMarker = "";
+            bool isAWin = false;
+            Button button = (Button)sender;
+            if (turnCount % 2 == 0)
+                currentMarker = button.Text = "X";
+            else
+                currentMarker = button.Text = "O";
+            button.Enabled = false;
+            turnCount++;
+            if (turnCount > 4)
+                isAWin = CheckWinningConditions(currentMarker);
+            if (isAWin == true)
+            {
+                ResultLabel.Text = $"{currentMarker} Wins!!!";
+                ResultLabel.Visible = true;
+                DisableAllControls();
+            }
+            else if (turnCount == 9)
+            {
+                ResultLabel.Text = "Its a draw!!";
+                ResultLabel.Visible = true;
+            }
+        }
+
+        private void DisableAllControls()
+        {
+            foreach (Control control in Panel1.Controls)
+            {
+                if (control.GetType() == typeof(Button))
+                    ((Button)control).Enabled = false;
+            }
+        }
+
+        private bool CheckWinningConditions(string currentMarker)
+        {
+            if (CheckHorizontalCondition(currentMarker) == true)
+                return true;
+            if (CheckVerticalCondition(currentMarker) == true)
+                return true;
+            if (CheckDiagonalConditions(currentMarker) == true)
+                return true;
+            return false;
+        }
+
+        private bool CheckHorizontalCondition(string currentMarker)
+        {
+            if (GameControl1.Text.Equals(currentMarker) && GameControl2.Text.Equals(currentMarker) && GameControl3.Text.Equals(currentMarker))
+                return true;
+            if (GameControl4.Text.Equals(currentMarker) && GameControl5.Text.Equals(currentMarker) && GameControl6.Text.Equals(currentMarker))
+                return true;
+            if (GameControl7.Text.Equals(currentMarker) && GameControl8.Text.Equals(currentMarker) && GameControl9.Text.Equals(currentMarker))
+                return true;
+            return false;
+        }
+
+        private bool CheckVerticalCondition(string currentMarker)
+        {
+            if (GameControl1.Text.Equals(currentMarker) && GameControl4.Text.Equals(currentMarker) && GameControl7.Text.Equals(currentMarker))
+                return true;
+            if (GameControl2.Text.Equals(currentMarker) && GameControl5.Text.Equals(currentMarker) && GameControl8.Text.Equals(currentMarker))
+                return true;
+            if (GameControl3.Text.Equals(currentMarker) && GameControl6.Text.Equals(currentMarker) && GameControl9.Text.Equals(currentMarker))
+                return true;
+            return false;
+        }
+
+        private bool CheckDiagonalConditions(string currentMarker)
+        {
+            if (GameControl1.Text.Equals(currentMarker) && GameControl5.Text.Equals(currentMarker) && GameControl9.Text.Equals(currentMarker))
+                return true;
+            if (GameControl3.Text.Equals(currentMarker) && GameControl5.Text.Equals(currentMarker) && GameControl7.Text.Equals(currentMarker))
+                return true;
+            return false;
+        }
+
+        protected void Reset_Click(object sender, EventArgs e)
+        {
+            foreach (Control control in Panel1.Controls)
+            {
+                if (control.GetType() == typeof(Button))
+                {
+                    var button = (Button)control;
+                    button.Enabled = true;
+                    button.Text = "";
+                    ResultLabel.Text = "";
+                    ResultLabel.Visible = false;
+                    turnCount = 0;
+                }
+            }
+        }
+    }
+}
